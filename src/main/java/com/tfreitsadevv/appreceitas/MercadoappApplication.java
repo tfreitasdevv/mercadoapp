1c870e89f96c1fa2920c0b7e9292aa6be3853022
Thiago Freitas <tfreitasdevv@gmail.com>
2019-07-19 17:51:22 -0300

    Projeto criado

---
+++ src/main/java/com/tfreitsadevv/appreceitas/MercadoappApplication.java
@@ -0,0 +1,13 @@
+package com.tfreitsadevv.appreceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoappApplication.class, args);
+	}
+
+}
